5229202b90ab1a017181fee39a8bd6e0401326c8
Martin Garnica Calderon <martingc@MacBook-Pro-de-Martin.local>
2023-07-21 22:30:53 -0500

    Navbar animation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,31 @@
-import { Logo, Socials } from '@/components';
+import { useEffect, useState } from "react";
+import { Logo, Socials } from "@/components";
 
 const Navbar = () => {
+  const [scroll, setScroll] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrolled = window.scrollY > 0;
+      setScroll(isScrolled);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className='fixed z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]'>
-      <div className='container mx-auto'>
-        <div className='flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8'>
+    <nav
+      className={`fixed z-50 w-full flex items-center px-16 xl:px-0 ${
+        scroll
+          ? "bg-neutral-800 text-neutral-200 xl:h-[70px]"
+          : "bg-transparent text-neutral-800 xl:h-[90px]"
+      } transition-all duration-300`}
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row justify-between items-center">
           {/* logo */}
           <Logo />
           {/* socials */}
